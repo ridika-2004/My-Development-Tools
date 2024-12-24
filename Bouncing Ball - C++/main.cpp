4daf346e0f35c8b9cd73d6a09f7a89e96219a922
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2024-12-24 23:18:49 +0600

    ball stuck at platforms!!

--- Bouncing Ball - C++/main.cpp
+++ Bouncing Ball - C++/main.cpp
@@ -8,9 +8,19 @@ class Ball{
     float size;
 };
 
-int ball_on_ground(Ball ball,int window_height){
-    if(ball.y>=window_height-ball.size){
-        return 1;
+class Platform{
+    public:
+    int x,y,width,height;
+};
+int ball_on_platform(Ball ball,Platform platforms[]){
+    int count = 3;
+    for(int i=0;i<count;i++){
+        Vector2 ball_position = {.x = ball.x, .y=ball.y};
+        Rectangle platform_rec = {.x = platforms[i].x, .y = platforms[i].y,
+        .width = platforms[i].width, .height = platforms[i].height};
+        if(CheckCollisionCircleRec(ball_position, ball.size, platform_rec)){
+            return 1;
+        }
     }
     return 0;
 }
@@ -19,16 +29,32 @@ int main(){
     int window_height = 600,window_width =800;
     int gravity =3;
     Ball ball;
+    Platform platforms[3];
     ball.x= window_width/2, ball.y = window_height/2,ball.size = 40.0,ball.velocity=4;
+    platforms[0].x = 0;
+    platforms[0].y = window_height-10;
+    platforms[0].width = window_width;
+    platforms[0].height = 10;
+
+    platforms[1].x = abs(window_width*0.1);
+    platforms[1].y = abs(window_height*0.7);
+    platforms[1].width = abs(window_width*0.3);
+    platforms[1].height = abs(window_height*0.05);
+
+    platforms[2].x = abs(window_width*0.3);
+    platforms[2].y = abs(window_height*0.5);
+    platforms[2].width = abs(window_width*0.3);
+    platforms[2].height = abs(window_height*0.05);
+
     SetTargetFPS(60);
     InitWindow(window_width,window_height,"Bouncing Ball");
 
     while(!WindowShouldClose()){
         BeginDrawing();
 
-        if(ball_on_ground(ball,window_height)){
+        if(ball_on_platform(ball,platforms)){
             ball.velocity=0;
-            ball.y = window_height - ball.size;
+            //ball.y = window_height - ball.size;
 
             if(IsKeyPressed(KEY_SPACE)){
                 ball.velocity = -30;
@@ -45,8 +71,17 @@ int main(){
 
         ClearBackground(RAYWHITE);
 
-        DrawRectangle(abs(window_width*0.1), abs(window_height*0.7),abs(window_width*0.2),abs(window_height*0.05),DARKBROWN);
-        //ass
+        int count = sizeof(platforms)/sizeof(Platform);
+        for(int i=0;i<count;i++){
+            DrawRectangle(
+                platforms[i].x,
+                platforms[i].y,
+                platforms[i].width,
+                platforms[i].height,
+                DARKBROWN);
+        }
+
+        
         DrawCircle(ball.x,ball.y,ball.size,RED);
 
         EndDrawing();
