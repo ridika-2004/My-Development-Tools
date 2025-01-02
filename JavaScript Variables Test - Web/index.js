325603c9b67275daeebf5c3e333d4af3a460d978
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-02 18:57:42 +0600

    added js variables

---
+++ JavaScript Variables Test - Web/index.js
@@ -0,0 +1,7 @@
+let name = "Ridika";
+let isStudent = true;
+let favLanguage = "C++";
+
+document.getElementById("p1").textContent = `My name is ${name}`;
+document.getElementById("p2").textContent = `Am I a student? ${isStudent}`;
+document.getElementById("p3").textContent = `My favourite language for programming is ${favLanguage}`;
