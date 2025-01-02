837331965c31ee0ccca0c85deb3aab489e0d838b
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-02 13:51:28 +0600

    added 2 js files

---
+++ First JavaScript Test - Web/index.js
@@ -0,0 +1,6 @@
+// console.log(`Hello`);
+
+window.alert(`This is an alert`);
+
+document.getElementById("myh1").textContent = `Hello`;
+document.getElementById("myp").textContent = `I'm learning javascript`;
---
+++ JavaScript Button Test - Web/script.js
@@ -0,0 +1,3 @@
+document.getElementById("testButton").addEventListener("click", function() {
+    document.getElementById("message").innerText = "JavaScript is working!";
+});
