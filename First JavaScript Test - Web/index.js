18a57827f7d2fb262a0e10ad777639bde19f7d99
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-03 09:50:34 +0600

    Deleted First JavaScript Test - Web directory

--- First JavaScript Test - Web/index.js
+++
@@ -1,6 +0,0 @@
-// console.log(`Hello`);
-
-window.alert(`This is an alert`);
-
-document.getElementById("myh1").textContent = `Hello`;
-document.getElementById("myp").textContent = `I'm learning javascript`;
