336c1f844d329d44488c0a5d21cfb2892e590551
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-03 09:52:48 +0600

    Deleted JavaScript User Input Test - Web directory

--- JavaScript User Input Test - Web/index.js
+++
@@ -1,8 +0,0 @@
-let name;
-//name = window.prompt("What's your username?");
-
-document.getElementById("mysubmit").onclick = function(){
-    name = document.getElementById("mytext").value;
-    //console.log(name);
-    document.getElementById("myh1").textContent = `Hello ${name}`;
-}
