086ff0c2e03ba62825def834e4caf7a0df5ca592
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-03 09:52:22 +0600

    Deleted JavaScript Variables Test - Web directory

--- JavaScript Variables Test - Web/index.js
+++
@@ -1,7 +0,0 @@
-let name = "Ridika";
-let isStudent = true;
-let favLanguage = "C++";
-
-document.getElementById("p1").textContent = `My name is ${name}`;
-document.getElementById("p2").textContent = `Am I a student? ${isStudent}`;
-document.getElementById("p3").textContent = `My favourite language for programming is ${favLanguage}`;
