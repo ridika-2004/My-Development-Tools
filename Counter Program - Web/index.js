21910ce67bb8fca42bf488ea4696db312bafe143
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-04 18:15:51 +0600

    completed

---
+++ Counter Program - Web/index.js
@@ -0,0 +1,22 @@
+const decreasebtn = document.getElementById("decreasebtn");
+const increasebtn = document.getElementById("increasebtn");
+const resetbtn = document.getElementById("resetbtn");
+
+const countLabel = document.getElementById("countlabel");
+
+let count=0;
+
+increasebtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decreasebtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetbtn.onclick = function(){
+    count =0;
+    countLabel.textContent = count;
+}
