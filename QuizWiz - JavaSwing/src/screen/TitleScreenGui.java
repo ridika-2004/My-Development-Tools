184a29a36f254fe8b1f63a1bb3bf66c2e1c5348a
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-06 21:14:01 +0600

    added titlescreengui

---
+++ QuizWiz - JavaSwing/src/screen/TitleScreenGui.java
@@ -0,0 +1,14 @@
+package screen;
+
+import javax.swing.*;
+
+public class TitleScreenGui extends JFrame {
+    public TitleScreenGui(){
+        super("Title Screen");
+        setSize(400,565);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
