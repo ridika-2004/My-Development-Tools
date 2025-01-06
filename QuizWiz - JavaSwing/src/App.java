90e51044d5f9cb0613f62ee2ec153367725c25c6
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-06 21:16:37 +0600

    added app class

---
+++ QuizWiz - JavaSwing/src/App.java
@@ -0,0 +1,14 @@
+import screen.TitleScreenGui;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TitleScreenGui().setVisible(true);
+            }
+        });
+    }
+}
