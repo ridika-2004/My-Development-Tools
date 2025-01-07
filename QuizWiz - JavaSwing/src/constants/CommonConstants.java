2e5555c57b9c00025db487197e4dee7c27fe332b
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-07 18:46:38 +0600

    added colors in commonconstants class

--- QuizWiz - JavaSwing/src/constants/CommonConstants.java
+++ QuizWiz - JavaSwing/src/constants/CommonConstants.java
@@ -1,4 +1,11 @@
 package constants;
 
+import java.awt.*;
+
 public class CommonConstants {
+    public static final Color light_blue = Color.decode("#5699FD");
+    public static final Color dark_blue = Color.decode("#45699E");
+    public static final Color bright_yellow = Color.decode("#B7FF5B");
+    public static final Color light_red = Color.decode("#FF8585");
+    public static final Color light_green = Color.decode("#76F592");
 }
