7f370303ced8e40562a5078863b27d3620134677
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-08 07:53:10 +0600

    added createques title

--- QuizWiz - JavaSwing/src/screen/CreateQuesScreenGui.java
+++ QuizWiz - JavaSwing/src/screen/CreateQuesScreenGui.java
@@ -3,6 +3,7 @@ package screen;
 import constants.CommonConstants;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CreateQuesScreenGui extends JFrame {
     public CreateQuesScreenGui(){
@@ -13,5 +14,16 @@ public class CreateQuesScreenGui extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         getContentPane().setBackground(CommonConstants.offwhite);
+        addGuiComponents();
     }
+
+    private void addGuiComponents(){
+        JLabel titleLabel = new JLabel("Create Your Own Question");
+        titleLabel.setFont(new Font("Arial",Font.BOLD,24));
+        titleLabel.setBounds(50,15,310,29);
+        //titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setForeground(CommonConstants.dark_blue);
+        add(titleLabel);
+    }
+
 }
