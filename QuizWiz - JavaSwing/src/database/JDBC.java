9daa32d3b772f30aecc15e17b9f9c5f707b1244b
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-12 07:56:41 +0600

    did smth idk

--- QuizWiz - JavaSwing/src/database/JDBC.java
+++ QuizWiz - JavaSwing/src/database/JDBC.java
@@ -2,6 +2,8 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class JDBC {
     private static final String dburl = "jdbc:mysql://127.0.0.1/quizwiz";
@@ -11,10 +13,45 @@ public class JDBC {
     private static boolean insertQuesToDB(String question, String category, String[] answer, int correctIndex){
         try {
             Connection connection = DriverManager.getConnection(dburl,dbusername,dbpass);
-
+            Category categoryObj = getCategory(category);
+            if(categoryObj==null){
+                categoryObj = insertCategory(category);
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
+    private static Category getCategory(String category){
+        try{
+            Connection connection = DriverManager.getConnection(dburl,dbusername,dbpass);
+            PreparedStatement getCategoryQuery = connection.prepareStatement("select * from category where categorycol = ?");
+            getCategoryQuery.setString(1,category);
+            ResultSet resultSet = getCategoryQuery.executeQuery();
+
+            if(resultSet.next()){
+                int categoryId = resultSet.getInt("categoryid");
+                return new Category(categoryId,category);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private static Category insertCategory(String category){
+        try{
+            Connection connection = DriverManager.getConnection(dburl,dbusername,dbpass);
+            PreparedStatement getCategoryQuery = connection.prepareStatement("select * from category where categorycol = ?");
+            getCategoryQuery.setString(1,category);
+            ResultSet resultSet = getCategoryQuery.executeQuery();
+
+            if(resultSet.next()){
+                int categoryId = resultSet.getInt("categoryid");
+                return new Category(categoryId,category);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
