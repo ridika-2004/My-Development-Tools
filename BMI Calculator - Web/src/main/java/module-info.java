be13add0f23c31afa43377ef48b3db87e30fb4a6
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-19 09:26:44 +0600

    Deleted BMI Calculator - Web directory

--- BMI Calculator - Web/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bmi_calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bmi_calculator to javafx.fxml;
-    exports com.example.bmi_calculator;
-}
