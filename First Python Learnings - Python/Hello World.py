61a7dbc165d39be8ed095a07a17d232c857fa86c
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-25 18:52:28 +0600

    learnt till strings

--- First Python Learnings - Python/Hello World.py
+++ First Python Learnings - Python/Hello World.py
@@ -1,4 +1,5 @@
 import sys
+import random
 print(sys.version)
 
 print("Hello Ridika!")
@@ -12,5 +13,51 @@ number = 5.3
 
 print("Today's earthqauke in richter scale was", number, ", Scary right?")
 
-print(type(number))
+x = memoryview(bytes(5))
+print(type(x))
 
+mylist = ["apple", "banana", "cherry", 1, 2, 3, True, False]
+print(len(mylist))
+print(mylist[3])
+
+x = mylist
+x[4] = "orange"
+print(x[4])
+
+i = "i "
+am = "am "
+five = 5
+cool = "cool"
+print(i+am+cool)
+print(i+am,five)
+
+def function():
+    print("This is a function")
+
+function()
+
+v = random.randrange(1,20)
+print(v)
+
+s = str(v)
+print(s)
+
+for x in "ridika":
+    print(x)
+
+print('r' in "ridika")
+
+if 'r' in "ridika":
+    print("Yes, 'r' is present in the string")
+else:
+    print("No, 'r' is not present in the string")
+
+b = "Hello, World!  "
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+print(b[-5:-2])
+print(b.upper())
+print(b.strip())
+print(b.replace("H", "J"))
+print(b.split(","))
