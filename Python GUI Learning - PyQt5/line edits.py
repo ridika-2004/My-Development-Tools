8b48bf2e88cfc50230ef5a026d167247639dcc54
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-02-06 22:29:53 +0600

    completed line edits

--- Python GUI Learning - PyQt5/line edits.py
+++ Python GUI Learning - PyQt5/line edits.py
@@ -17,8 +17,8 @@ class mainwindow(QMainWindow):
         self.setWindowIcon(QIcon("cool.png"))
         self.setStyleSheet("background-color : #191c27")
 
-        self.label = QLabel("Input a noun", self)
-        self.label.setGeometry(500,50,500,100)
+        self.label = QLabel("Enter your name", self)
+        self.label.setGeometry(100,50,1700,100)
 
         self.line_edit = QLineEdit(self)
         self.button = QPushButton("Submit",self)
@@ -27,16 +27,17 @@ class mainwindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.line_edit.setGeometry(500,350,340,70)
+        self.line_edit.setGeometry(100,350,340,70)
         self.line_edit.setStyleSheet("background-color : #ffffff; font-size : 30px")
-        self.label.setStyleSheet("font-size : 80px; color : #ffffff; font-family : Arial; font-weight : bold;")
+        self.label.setStyleSheet("font-size : 70px; color : #ffffff; font-family : Arial; font-weight : bold;")
 
-        self.button.setGeometry(850,350,100,70)
+        self.button.setGeometry(450,350,100,70)
         self.button.setStyleSheet("background-color : #b2cded;")
         self.button.clicked.connect(self.submit)
 
     def submit(self):
-        pass
+        name = self.line_edit.text()
+        self.label.setText("Hello "+name+"! Hope you become happy in your life")
 
     
 
