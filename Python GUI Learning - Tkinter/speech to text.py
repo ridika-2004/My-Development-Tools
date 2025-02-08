66162ca8239bbd0214b006cd550532582d60177c
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-02-08 11:13:20 +0600

    added splash screen here

--- Python GUI Learning - Tkinter/speech to text.py
+++ Python GUI Learning - Tkinter/speech to text.py
@@ -4,7 +4,12 @@ import speech_recognition as sr
 def speak():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
+        splash_window = Tk()
+        splash_window.title("Speaking window")
+        splash_window.geometry("900x200")
         # Initial label update
+        label = Label(splash_window)
+        label.pack(pady=20)         
         label.config(text="Adjusting for ambient noise... Please wait.")
         window.update_idletasks()  # Force update the UI immediately
         recognizer.adjust_for_ambient_noise(source)
@@ -23,15 +28,19 @@ def speak():
             # label.config(text=words)
             window.update_idletasks()
             text.insert(END, words+" ")
-            label.config(text="")
+            # label.config(text="")
+            splash_window.destroy()
         except sr.UnknownValueError:
             label.config(text="Google API could not understand the audio.")
             window.update_idletasks()
-            label.config(text="")
+            # label.config(text="")
+            splash_window.after(2000,splash_window.destroy)
         except sr.RequestError as e:
             label.config(text=f"Could not request results from Google API; {e}")
             window.update_idletasks()
-            label.config(text="")
+            # label.config(text="")
+            splash_window.after(2000,splash_window.destroy)
+
 
 # GUI Setup
 window = Tk()
@@ -41,9 +50,6 @@ window.geometry("500x500")
 # App Icon (Ensure the file exists)
 icon = PhotoImage(file="cool.png")  # Make sure you have this image file or comment out this line
 window.iconphoto(True, icon)
-
-label = Label(window,width=500)
-label.pack(pady=20)
 
 text = Text(window, height=10, width=50)
 text.pack(pady=10)
